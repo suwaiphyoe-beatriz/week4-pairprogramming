7835e283455cd6dfb897962b2a15931a4c047f96
Su Wai Phyoe <beatrizisme4@gmail.com>
2025-09-08 15:37:31 +0300

    add morgan to app.js

--- week4-be-pp/app.js
+++ week4-be-pp/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
+const morgan = require('morgan');
+
 
 // Middleware to parse JSON
 app.use(express.json());
-
+app.use(morgan('tiny'));
 // Use the carRouter for all /cars routes
 //app.use('/tours', tourRouter);
 
